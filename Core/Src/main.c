396313031fb51f8bb7fa8a59847bb7c19c5d4ea4
PeaceChanpornpakdee <yothinpeace@gmail.com>
2021-03-11 13:31:34 +0700

    Interrupt Code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -315,6 +315,13 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	if(GPIO_Pin == GPIO_PIN_13)
+	{
+		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	}
+}
 
 /* USER CODE END 4 */
 
